0e38f9aa7ab4d5230c0dfb9aba9ce6d84c7f15dd
Bundit Hirunwongsawang <bundit.hir@gmail.com>
2022-10-17 02:20:02 +0700

    first

---
+++ script.js
@@ -0,0 +1,37 @@
+var ic_name = document.getElementById("ic-er-name");
+var ic_last = document.getElementById("ic-er-last");
+var ic_mail = document.getElementById("ic-er-email");
+var ic_pass = document.getElementById("ic-er-pass");
+
+var name_err = document.getElementById("name");
+var last_err = document.getElementById("last");
+var email_err = document.getElementById("email");
+var pass_err = document.getElementById("pass");
+
+var in_name = document.getElementById("f-name");
+var in_last = document.getElementById("f-last");
+var in_email = document.getElementById("f-email");
+var in_pass = document.getElementById("f-pass");
+
+var btn =document.getElementById("btn");
+
+const valid =(e)=>{
+    e.preventDefault();
+    const icon = [ic_name,ic_last,ic_mail,ic_pass];
+    const input = [in_name,in_last,in_email,in_pass];
+    const text = [name_err,last_err,email_err,pass_err];
+    for(let i=0;i<=3 ;i++ ){
+        let value = input[i].value;
+        if(value == '' || value === undefined){
+            icon[i].style.display ="block";
+            text[i].style.display = "block";
+        }
+    else{
+        icon[i].style.display ="none";
+            text[i].style.display = "none";
+    }
+    }
+}
+
+
+btn.addEventListener('click',valid);
